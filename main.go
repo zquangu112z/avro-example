b5e48fa373a0f0f3e8f173b9dd303926a51d8802
Ngữ Quang Trương <zquangu112z@gmail.com>
2018-09-27 18:24:11 +0700

    test: try to serialize  #WIP status: failed

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/actgardner/gogen-avro/container"
 	"github.com/linkedin/goavro"
 	"github.com/zquangu112z/avro-example/avro"
 )
@@ -14,39 +16,57 @@ var (
 func main() {
 
 	//Sample Data
-	observation := avro.Observation{}
-	fmt.Println(observation.Schema())
-
-	diagnosticReport := avro.DiagnosticReport{}
-	fmt.Println(diagnosticReport.Schema())
+	observation := avro.Observation{
+		Identifier:           nil,
+		BaseOn:               nil,
+		Status:               "fine",
+		Category:             nil,
+		Code:                 nil,
+		Subject:              nil,
+		Context:              nil,
+		EffectiveDateTime:    "today",
+		EffectivePeriod:      nil,
+		Issued:               "ABX",
+		Performer:            nil,
+		ValueQuantity:        nil,
+		ValueCodeableConcept: nil,
+		ValueString:          "nil",
+		ValueBoolean:         "nil",
+		ValueRange:           nil,
+		ValueTime:            "nil",
+		ValueDateTime:        "nil",
+		ValuePeriod:          nil,
+	}
+	// fmt.Println(observation.Schema())
+	fmt.Println("-----------")
 
 	// Open a file to write
-	// fileWriter, err := os.Create("bundle.avro")
-	// if err != nil {
-	// 	fmt.Printf("Error opening file writer: %v\n", err)
-	// 	return
-	// }
+	fileWriter, err := os.Create("observation.avro")
+	if err != nil {
+		fmt.Printf("Error opening file writer: %v\n", err)
+		return
+	}
 
-	// containerWriter, err := avro.NewTwitter_schemaWriter(fileWriter, container.Null, 10)
-	// if err != nil {
-	// 	fmt.Printf("Error opening container writer: %v\n", err)
-	// 	return
-	// }
+	containerWriter, err := avro.NewObservationWriter(fileWriter, container.Null, 10)
+	if err != nil {
+		fmt.Printf("Error opening container writer: %v\n", err)
+		return
+	}
 
-	// // Write the record to the container file
-	// err = containerWriter.WriteRecord(&twitter_schema)
-	// if err != nil {
-	// 	fmt.Printf("Error writing record to file: %v\n", err)
-	// 	return
-	// }
+	// Write the record to the container file
+	err = containerWriter.WriteRecord(&observation)
+	if err != nil {
+		fmt.Printf("Error writing record to file: %v\n", err)
+		return
+	}
 
-	// // Flush the buffers to ensure the last block has been written
-	// err = containerWriter.Flush()
-	// if err != nil {
-	// 	fmt.Printf("Error flushing last block to file: %v\n", err)
-	// 	return
-	// }
+	// Flush the buffers to ensure the last block has been written
+	err = containerWriter.Flush()
+	if err != nil {
+		fmt.Printf("Error flushing last block to file: %v\n", err)
+		return
+	}
 
-	// fileWriter.Close()
+	fileWriter.Close()
 
 }
--- makefile
+++ makefile
@@ -1,11 +1,11 @@
-test:
-	rm -rf avro &&  mkdir avro && ~/go/bin/gogen-avro --containers  avro schemas/*.avsc && go run main.go
-
 idl:
-	rm -rf output && mkdir output && java -jar /Users/kilia/Downloads/avro-tools-1.8.2.jar idl2schemata idl/datatypes.identifier.avdl output
-
-final:
-	rm -rf output && mkdir output && java -jar /Users/kilia/Downloads/avro-tools-1.8.2.jar idl2schemata idl/datatypes.identifier.avdl output && rm -rf avro &&  mkdir avro && ~/go/bin/gogen-avro --containers  avro output/Identifier.avsc && go run main.go
+	rm -rf output && mkdir output && java -jar /Users/kilia/Downloads/avro-tools-1.8.2.jar idl2schemata idl/main.avdl output
 
 output:
-	rm -rf avro &&  mkdir avro && ~/go/bin/gogen-avro --containers  avro output/*.avsc && go run main.go
+	rm -rf avro &&  mkdir avro && ~/go/bin/gogen-avro --containers  avro output/Main.avsc && go run main.go
+
+test:
+	rm -rf output && mkdir output && java -jar /Users/kilia/Downloads/avro-tools-1.8.2.jar idl2schemata idl/main.avdl output && rm -rf avro &&  mkdir avro && ~/go/bin/gogen-avro --containers  avro output/Main.avsc && go run main.go
+
+clean:
+	rm -rf ./avro && rm -rf ./output
